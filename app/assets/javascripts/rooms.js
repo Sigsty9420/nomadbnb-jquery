c71ff56c6a2ba66ec536d517026e7ff595579380
bortch89 <bortch@gmail.com>
2017-06-21 15:07:21 +0200

    Fixed bug of div not being populated

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,14 +1,18 @@
 $(function(){
   $("a.load_reviews").on("click", function(e){
-    $.ajax({
-      debugger;
-      method: "GET",
-      URL: this.href
-    }).success( function(response) {
-      $("div.comments").html(response)
-    }).error(function(){
-      alert("Error loading reviews")
-    });
+//    $.ajax({
+//      debugger;
+//      method: "GET",
+//      URL: this.href
+//    }).success( function(response) {
+//      $("div.comments").html(response)
+//    }).error(function(){
+//      alert("Error loading reviews")
+//    });
+
+    $.get(this.href).success(function(response){
+      $("div.reviews").html(response)
+    })
 
     e.preventDefault();
   })
