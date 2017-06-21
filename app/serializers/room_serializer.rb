cbbabd62c71eb2c90981ee8845c78e3ac8bb5087
bortch89 <bortch@gmail.com>
2017-06-21 12:56:23 +0200

    Added has_many relationship for rooms serializer

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -1,4 +1,5 @@
 class RoomSerializer < ActiveModel::Serializer
   attributes :id, :home_type, :room_type, :capacity, :bed_rooms, :bath_rooms, :listing_name, :summary, :address, :price, :active
   has_many :reservations
+  has_many :reviews
 end
